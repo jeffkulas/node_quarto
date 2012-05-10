e5f6507138f4e300f40fcd087bfcc3f7e57399e7
Jeff Kulas <jeffrey.m.kulas@gmail.com>
2012-05-09 23:56:13 -0400

    apply module pattern to gamelogic

--- public/javascripts/gamelogic.js
+++ public/javascripts/gamelogic.js
@@ -1,48 +1,50 @@
 var exports;
-var GAME = exports || {};
+var GAME = (function (my) {
+	var player1 = "player1"
+	  , player2 = "player2"
+	  , waitingOn = ""
+	  , messages = ["Anyone may select a piece to start the game.",
+									"p$ selected a piece. Anyone else may play the piece.",
+									"p$ turn to select a piece.",
+									"p$ turn to play the selected piece."];
+		my.playHistory = [];
+		my.selectedPieceId = null;
+		my.moveType = "select"; //select or place
 
-GAME.playHistory = [];
-GAME.player1 = "player1";
-GAME.player2 = "player2";
-GAME.waitingOn = "";
-GAME.moveType = "select"; //select or place
-GAME.selectedPieceId = null;
-GAME.messages = ["Anyone may select a piece to start the game.",
-			"p$ selected a piece. Anyone else may play the piece.",
-			"p$ turn to select a piece.",
-			"p$ turn to play the selected piece."
-			];
-GAME.makeMove = function(mv) {
-		//*** validate the move
-		console.log(mv.moveType);
-		this.playHistory.push(mv);
-		switch (mv.moveType) {
-			case "select":
-				this.moveType = "place";
-				if (mv.whoName === this.player1 && this.player2 != "player2") {
-					waitingOn = this.player2;
-				} else {
-					if (this.player2 != "player2") {
-						waitingOn = this.player1;
+		my.makeMove = function(mv) {
+			//*** validate the move
+			this.playHistory.push(mv);
+			switch (mv.moveType) {
+				case "select":
+					this.moveType = "place";
+					if (mv.whoName === this.player1 && this.player2 != "player2") {
+						waitingOn = this.player2;
+					} else {
+						if (this.player2 != "player2") {
+							waitingOn = this.player1;
+						}
 					}
-				}
-				this.selectedPieceId = mv.pieceId;
-				break;
-			case "place": 
-				this.moveType = "select";
-				this.selectedPieceId = null;
-				break;
-			case "reset":
-				this.restart();
-		}
-		
-	};
-GAME.restart = function() {
-		console.log('restart called');
-		this.playHistory = [];
-		this.player1 = "player1";
-		this.player2 = "player2";
-		this.waitingOn = "";
-		this.turnType = "select";
-		this.selectedPiece = null;
-	};
+					this.selectedPieceId = mv.pieceId;
+					break;
+				case "place": 
+					this.moveType = "select";
+					this.selectedPieceId = null;
+					break;
+				case "reset":
+					this.restart();
+			}
+		};
+
+		my.restart = function() {
+			console.log('restart called');
+			this.playHistory = [];
+			this.player1 = "player1";
+			this.player2 = "player2";
+			this.waitingOn = "";
+			this.turnType = "select";
+			this.selectedPiece = null;
+		};
+
+	return my;
+      
+}) (exports || {});
