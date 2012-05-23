c9a2694d540000861f59c611fd045cc8310162c6
Jeff Kulas <jeffrey.m.kulas@gmail.com>
2012-05-23 00:15:10 -0400

    fix resizing

--- public/javascripts/gameui.js
+++ public/javascripts/gameui.js
@@ -9,6 +9,8 @@ function move(obj) {
     GAME.playHistory = obj.playHistory;
     // update the board using the history
     for ( var i=0, len=obj.playHistory.length; i<len; ++i ){
+      var move = obj.playHistory[i];
+      move.speed = 1;
       updateBoard(obj.playHistory[i]);
     }
     if (GAME.playHistory[GAME.playHistory.length-1].moveType == "select") {
@@ -62,32 +64,28 @@ function placeSelected(placeObj) {
 }
 
 function updateBoard(move) {
-  console.log('updateBoard ' + move.pieceId);
+  var moveSpeed = 600;
+  if (move.speed) moveSpeed = move.speed;
   switch (move.moveType) {
     case "select":
-      var $movePiece = $("#" + move.pieceId);
-      $movePiece.addClass("played");
+      $("#" + move.pieceId).addClass("played");
       var selectedOffset = $("#selected").offset();
-      var pieceOffset = $movePiece.offset();
-      $movePiece.addClass("moving");
-      $movePiece.animate({top: "+=" + (selectedOffset.top - pieceOffset.top + 1) + "px",
-        left: "+=" + (selectedOffset.left - pieceOffset.left + 1) + "px"}, 600);
+      var pieceOffset = $("#" + move.pieceId).offset();
+      $("#" + move.pieceId).animate({top: "+=" + (selectedOffset.top - pieceOffset.top + 1) + "px",
+        left: "+=" + (selectedOffset.left - pieceOffset.left + 1) + "px"}, moveSpeed);
       break;
     case "place":
-      console.log('move.place ' + move.place);
       $("#" + move.place).addClass("used");
       var selectedOffset = $("#selected").offset();
       var placeOffset = $("#" + move.place).offset();
-      console.log(placeOffset);
       $("#" + move.pieceId).animate({top: "+=" + (placeOffset.top - selectedOffset.top ) + "px",
-      left: "+=" + (placeOffset.left - selectedOffset.left ) + "px"}, 600);
+      left: "+=" + (placeOffset.left - selectedOffset.left ) + "px"}, moveSpeed);
       break;
     case "reset":
       //move all pieces back to the unplayed div
       $("#unplayed > object").each(function(index) {
+        $(this).offset({top: "0px", left: "0px"});
         $(this).removeClass('played');
-        $(this).removeClass('moving');
-        $(this).animate({top: "0px", left: "0px"}, 300);
       });
         
       //set all spaces unplayed
@@ -106,33 +104,42 @@ function resetBoard() {
   socket.emit('move', move);
 }
 
-function resizeScreen() {
+function setLayout() {
   if ($(window).width() < $(window).height()) {
     $('#boardcont').removeClass('span5');
     $('#boardcont').addClass('span8');
     $('#unplayed').removeClass('span5');
     $('#unplayed').addClass('span8');
+    $('#status').removeClass('span2');
+    $('#status').addClass('span4');
   } else {
     $('#boardcont').addClass('span5');
     $('#boardcont').removeClass('span8');
     $('#unplayed').addClass('span5');
     $('#unplayed').removeClass('span8');    
+    $('#status').removeClass('span4');
+    $('#status').addClass('span2');
   }
-  //move played pieces to where they belong
-  // update the board using the history
-  // updateBoard({moveType:"reset"});
-  // for ( var i=0, len=GAME.playHistory.length; i<len; ++i ){
-  //   updateBoard(GAME.playHistory[i]);
-  // }
 }
 
+function resizeScreen() {
+  setLayout();
+  //move played pieces to where they belong
+  // update the board using the history
+  updateBoard({moveType:"reset"});
+  if (GAME.playHistory.length) move({playHistory: GAME.playHistory});
+}
+
+var resizeTimer;
+
 $(window).resize(function() {
-  resizeScreen();
+  clearTimeout(resizeTimer);
+  resizeTimer = setTimeout(resizeScreen, 5);  
 });
 
 
 $(function() {
   $('#newgamebutton').click(function() {resetBoard(); });   
   // rearrange board if player has vertical screen
-  resizeScreen();
+  setLayout();
 });
