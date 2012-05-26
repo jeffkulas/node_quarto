8db4c31aa235040f4a8436b3fe4ae5362a4a04fd
Jeff Kulas <jeffrey.m.kulas@gmail.com>
2012-05-26 00:37:56 -0400

    delete game instance from memory when all players disconnected

--- app.js
+++ app.js
@@ -37,53 +37,57 @@ app.get('/game', function(req, res) {
   res.redirect('/game/' + Math.floor(Math.random()*1000000));
 });
 
-//*** replace rooms with something from the database
+//*** replace games with something from the database
 var games = {};
 
 app.listen(3000);
 console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
 
-var io = sio.listen(app)
-  , playerID = 0;
+var io = sio.listen(app);
 
 app.get('/game/:id', function(req, res) {
-  console.log(req.params.id);
   //create game if not in memory (should just pull from mongodb)
   if (!games['game' + req.params.id]) {
+    console.log('creating game');
     var game = {};
     game.id = req.params.id;
+    // *** figure out how to instance a new game from a required object
     game.mem = require('./public/javascripts/gamelogic.js');
+    game.mem.restart();
+    game.socketCount = 0;
     game.socket = io.of('/' + game.id).on('connection', function(socket) {
       if (game.mem.playHistory.length > 0) socket.emit('move', {playHistory : game.mem.playHistory});
-      socket.playerID = playerID++; //*** player ID should be replaced by the socket session
-                                    //    or something else
+      game.socketCount++;
       
       socket.on('move', function(move) {
-          // *** validate the move in GAME object,
-          // make sure the move from is from the right session
-          socket.broadcast.emit('move', move);
-          game.mem.makeMove(move);
+        // *** validate the move in GAME object,
+        // make sure the move from is from the right session
+        game.mem.makeMove(move);
+        socket.broadcast.emit('move', move);
+        console.log('move   game:' + game.id + '  socket.id:' + socket.id);
+      });
+
+      socket.on('disconnect', function () {
+        //cleanup in memory game if last person out
+        game.socketCount--;
+        console.log('disconnect');
+        // *** deleting the game appears broken
+        if (game.socketCount == 0) {
+          console.log('deleting game');
+          delete game.mem;
+          delete game.socket;
+          delete io.namespaces['/' + game.id];
+          delete games['game' + game.id];
+          delete game;
+        }
+          
+
       });
 
     });
 
     games['game' + req.params.id] = game;
-  }
+  } 
 
   res.render('game', { title: 'Quarto', gameId: req.params.id });
 });
-  
-// io.sockets.on('connection', function(socket){
-//   if (GAME.playHistory.length > 0) socket.emit('move', {playHistory : GAME.playHistory});
-//   socket.playerID = playerID++;
-  
-//   socket.on('move', function(move) {
-//       // *** validate the move in GAME object,
-//       // make sure the move from is from the right session
-//       socket.broadcast.emit('move', move);
-//       GAME.makeMove(move);
-//   });
-
-// });
-
-
